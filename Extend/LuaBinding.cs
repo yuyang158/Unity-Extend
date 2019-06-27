8b62fd035357492054e3138653e0761bec5a2deb
yuyang <yuyangasd@gmail.com>
2019-06-27 16:48:09 +0800

    extend xlua unity component binding.

---
+++ Extend/LuaBinding.cs
@@ -0,0 +1,44 @@
+﻿using SerializableCollections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace XLua.Extend {
+    public class LuaBinding : MonoBehaviour {
+        public string luaFileName;
+
+        [Serializable]
+        public class LuaUnityBinding {
+            [SerializeField]
+            public Type type;
+            [SerializeField]
+            public MonoBehaviour go;
+        }
+
+        [Serializable]
+        public class StringGOSerializableDictionary : SerializableDictionary<string, LuaUnityBinding> {
+        }
+
+        public StringGOSerializableDictionary bindingContainer;
+
+        private LuaTable bindInstance;
+        void Awake() {
+            var ret = LuaVM.Default.LoadFileAtPath( luaFileName );
+            var classTable = ret[0] as LuaTable;
+            var constructor = classTable.Get<LuaFunction>( "new" );
+            bindInstance = constructor.Call( gameObject )[0] as LuaTable;
+
+            if( bindingContainer != null ) {
+                foreach( var item in bindingContainer ) {
+                    if( item.Value.go )
+                        bindInstance.SetInPath( item.Key, item.Value.go );
+                }
+            }
+
+            var awakeFunc = bindInstance.Get<LuaFunction>( "awake" );
+            awakeFunc.Call( bindInstance );
+        }
+    }
+}
+
+
---
+++ Extend/LuaVM.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace XLua.Extend {
+    public static class LuaVM {
+        static LuaVM() {
+            Default = new LuaEnv();
+            Default.customLoaders.Add( ( ref string filename ) => {
+                var asset = Resources.Load<TextAsset>( "Lua/" + filename + ".lua" );
+                return asset.bytes;
+            } );
+
+            Default.LoadFileAtPath( "class" );
+        }
+
+        public static LuaEnv Default { get; private set; }
+
+        public static object[] LoadFileAtPath( this LuaEnv env, string luaFileName ) {
+            return env.DoString( string.Format( "return require '{0}'", luaFileName ) );
+        }
+    }
+}
