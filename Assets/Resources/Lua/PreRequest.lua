b829ea4ce6d3e729a937a1050428ea5959f25526
yuyang <yuyangasd@gmail.com>
2020-01-08 12:14:35 +0800

    file replace

---
+++ Assets/Resources/Lua/PreRequest.lua
@@ -0,0 +1,67 @@
+package.cpath = package.cpath .. ';C:/Users/yang.yu/.Rider2019.2/config/plugins/intellij-emmylua/classes/debugger/emmy/windows/x64/?.dll'
+local dbg = require('emmy_core')
+-- dbg.tcpConnect('localhost', 9967)
+local next = next
+
+function string.split(self, delimiter)
+    local result = { }
+    local from = 1
+    local f, t = string.find(self, delimiter, from, true)
+    while f do
+        table.insert(result, string.sub(self, from, f - 1))
+        from = t + 1
+        f, t = string.find(self, delimiter, from, true)
+    end
+    table.insert(result, string.sub(self, from))
+    return result
+end
+
+function table.empty(t)
+    return next(t) == nil
+end
+
+local function print_r ( t )
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+table.print_r = print_r
+
+local SM = require "ServiceManager"
+local CS = require "ConfigService"
+local TS = require "TickService"
+
+SM.RegisterService(SM.SERVICE_TYPE.CONFIG, CS)
+SM.RegisterService(SM.SERVICE_TYPE.TICK, TS)
+
+TS.Register(function()
+end)
---
+++ Assets/Resources/Lua/UI/TestPanel.lua
@@ -0,0 +1,29 @@
+---@class UI.TestPanel
+---@field slot CS.Extend.AssetService.AssetReference @CS.UnityEngine.Sprite
+---@field slots UI.ItemSlot[]
+---@field txt CS.UnityEngine.UI.Text
+---@field btn CS.UnityEngine.UI.Button
+---@field num number
+---@field str string
+---@field stateSwitcher CS.Extend.Switcher.StateSwitcher
+local M = class()
+
+function M:ctor()
+    
+end
+
+function M:awake()
+    self.data = {
+        text = "1",
+        toggle = true
+    }
+    self.__CSBinding:SetDataContext(self.data)
+end
+
+function M:OnClick(evt)
+    local meta = getmetatable(self.data)
+    assert(meta and meta.__newindex)
+    self.data.text = tostring(math.tointeger(self.data.text) + 1)
+    self.data.toggle = not self.data.toggle
+end
+return M
