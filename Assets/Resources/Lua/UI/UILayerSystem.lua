f7f2287170796ce13770d38b59545b198182e20d
YuYang <yuyangasd@gmail.com>
2020-03-28 23:08:35 +0800

    add unity urp

--- Assets/Resources/Lua/UI/UILayerSystem.lua
+++ Assets/Resources/Lua/UI/UILayerSystem.lua
@@ -1,13 +1,16 @@
 ﻿local M = {}
 local layers = {}
+local table = table
 
 function M.Init(transform)
 	for i = 0, transform.childCount - 1 do
 		local childLayer = transform:GetChild(i)
 		table.insert(layers, {
 			layerTransform = childLayer,
+			layerGO = childLayer.gameObject,
 			name = childLayer.name
 		})
+		childLayer.gameObject:SetActive(false)
 	end
 end
 
