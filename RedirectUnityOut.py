d4f0d90c0892e94776265f3185c3db5ef700d685
YuYang <yuyangasd@gmail.com>
2020-04-14 20:55:08 +0800

    bug fix

--- RedirectUnityOut.py
+++ RedirectUnityOut.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os,sys,string,datetime,time,threading
- 
+import os
+import sys
+import string
+import datetime
+import time
+import threading
+
 g_bStop = False
+
+
 class OutputLogThread(threading.Thread):
     m_logFilePath = ''
+
     def run(self):
         global g_bStop
         nPosRead = 0
@@ -14,7 +22,7 @@ class OutputLogThread(threading.Thread):
             if os.path.isfile(self.m_logFilePath):
                 if fp == None:
                     fp = open(self.m_logFilePath, 'r')
- 
+
             if fp != None:
                 fp.seek(nPosRead)
                 allLines = fp.readlines()
@@ -24,25 +32,29 @@ class OutputLogThread(threading.Thread):
                 for lines in allLines:
                     print(lines)
             time.sleep(0.5)
- 
+
     def __init__(self, logPath):
         threading.Thread.__init__(self)
         self.m_logFilePath = logPath
- 
+
+
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print('not find unity path')
-		sys.exit(-1)
-	logFilePath = 'build.txt'
-	unityRunParm = ''
-	for i in range(len(sys.argv)):
-		if i > 0:
-			unityRunParm += ' ' + sys.argv[i]
-	unityRunParm += ' -logfile ' + logFilePath
-	if os.path.isfile(logFilePath):
-		os.remove(logFilePath)
-	logThread = OutputLogThread(logFilePath)
-	logThread.start()
-	os.system(unityRunParm)
-	g_bStop = True
-	logThread.join()
+    if len(sys.argv) < 2:
+        print('not find unity path')
+        sys.exit(-1)
+    logFilePath = 'build.txt'
+    unityRunParm = ''
+    for i in range(len(sys.argv)):
+        if i > 0:
+            if i == 1:
+                unityRunParm += ' "' + sys.argv[i] + '"'
+            else:
+                unityRunParm += ' ' + sys.argv[i]
+    unityRunParm += ' -logfile ' + logFilePath
+    if os.path.isfile(logFilePath):
+        os.remove(logFilePath)
+    logThread = OutputLogThread(logFilePath)
+    logThread.start()
+    os.system(unityRunParm)
+    g_bStop = True
+    logThread.join()
