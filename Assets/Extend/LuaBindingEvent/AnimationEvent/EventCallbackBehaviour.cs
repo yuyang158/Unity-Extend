dd94c142d555eab10cf3c2ba1a0979fb3750ac2a
YuYang <yuyangasd@gmail.com>
2020-05-04 20:33:18 +0800

    animation event driven by file

---
+++ Assets/Extend/LuaBindingEvent/AnimationEvent/EventCallbackBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Extend.LuaBindingEvent.AnimationEvent {
+	public class EventCallbackBehaviour : MonoBehaviour {
+		[Serializable]
+		private class EventInstanceEmmyFunction {
+			public EventInstance Event;
+			public LuaEmmyFunction Function;
+		}
+
+		[SerializeField]
+		private EventInstanceEmmyFunction[] Callbacks; 
+		
+		public void OnEvent(EventInstance instance) {
+		}
+	}
+}
---
+++ Assets/Extend/LuaBindingEvent/AnimationEvent/EventInstance.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Extend.LuaBindingEvent.AnimationEvent {
+	[CreateAssetMenu(fileName = "EventInstance", menuName = "Animation Event/Event Instance", order = 1)]
+	public class EventInstance : ScriptableObject {
+		public string EventName;
+		public EventParam Param;
+	}
+}
