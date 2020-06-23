168669e15c933d26f40ca90adeaa23ec3f8a6e00
yuyang158 <yuyang_158@hotmail.com>
2020-06-23 18:10:33 +0800

    global event collect list

---
+++ Assets/Extend/LuaBindingEvent/Editor/GlobalEventContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Extend.Common;
+using Extend.LuaBindingEvent.AnimationEvent;
+using UnityEngine;
+
+namespace Extend.LuaBindingEvent.Editor {
+	[CreateAssetMenu(fileName = "GlobalEventList", menuName = "Event/Editor Global Event List", order = 1)]
+	public class GlobalEventContext : ScriptableObject {
+		[Serializable]
+		public class Context {
+			public EventInstance Event;
+			public string Description;			
+		}
+
+		[ReorderList]
+		public Context[] Contexts;
+	}
+}
