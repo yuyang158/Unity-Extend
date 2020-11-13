191165a3ed8828ab3f013b346dfb98ea42200a1b
yuyang158 <yuyang_158@hotmail.com>
2020-11-13 15:16:56 +0800

    remove base url

--- admin-server/web/src/utils/request.js
+++ admin-server/web/src/utils/request.js
@@ -5,7 +5,6 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:3000/', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
