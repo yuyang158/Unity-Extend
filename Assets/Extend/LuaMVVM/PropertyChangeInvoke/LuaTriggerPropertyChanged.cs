165ae7f3462315e441471fce5d808f973b9e05a7
yuyang158 <yuyang_158@hotmail.com>
2021-03-17 14:53:11 +0800

    Add guaranteed lua callbacks

---
+++ Assets/Extend/LuaMVVM/PropertyChangeInvoke/LuaTriggerPropertyChanged.cs
@@ -0,0 +1,24 @@
+﻿using Extend.LuaUtil;
+using UnityEngine;
+using XLua;
+
+namespace Extend.LuaMVVM.PropertyChangeInvoke {
+	[RequireComponent(typeof(LuaBinding)), LuaCallCSharp]
+	public class LuaTriggerPropertyChanged : MonoBehaviour, IUnityPropertyChanged {
+		public event PropertyChangedAction OnPropertyChanged;
+		public object ProvideCurrentValue() {
+			return m_provideValueMethod.Invoke(m_binding.LuaInstance);
+		}
+
+		public void Trigger(object value) {
+			OnPropertyChanged?.Invoke(this, value);
+		}
+
+		private LuaBinding m_binding;
+		private GetLuaValue m_provideValueMethod;
+		private void Awake() {
+			m_binding = GetComponent<LuaBinding>();
+			m_provideValueMethod = m_binding.CachedClass.GetLuaMethod<GetLuaValue>("ProvideCurrentValue");
+		}
+	}
+}
