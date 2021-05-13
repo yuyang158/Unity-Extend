36cdcff1c516627e0ee56a9205ca99860ab704f4
yuyang158 <yuyang_158@hotmail.com>
2021-05-13 15:00:41 +0800

    modify ui load logic

--- Lua/UI/UIService.lua
+++ Lua/UI/UIService.lua
@@ -1,26 +1,32 @@
-﻿---@class UI.UIService
+---@class UI.UIService
 local M = {}
 local layers = {}
 local table, assert, typeof, pairs, ipairs = table, assert, typeof, pairs, ipairs
 local AssetService = CS.Extend.Asset.AssetService
 ---@type CS.Extend.Asset.AssetService
 local AssetServiceInstance = AssetService.Get()
-local Object = CS.UnityEngine.Object
 local UILayer = CS.Extend.UI.UILayer
-local sequence = require("base.action.sequence")
+---@type CS.Extend.UI.CloseOption
+local CloseOption = CS.Extend.UI.CloseOption
+---@type UI.UIViewContext[]
 local sortedElements = {}
-local UIViewBaseType = typeof(CS.Extend.UI.UIViewBase)
+---@type table<string, UI.UIViewContext>
+local contexts = {}
+local UIViewContext = require("UI.UIViewContext")
+local SM = require("ServiceManager")
 
 ---@type CS.Extend.UI.UIViewConfiguration
 local UIViewConfiguration
+local UIRoot
 
 function M.Init()
 	UIViewConfiguration = CS.Extend.UI.UIViewConfiguration.Load()
 	local ref = AssetServiceInstance:Load("UILayers", typeof(CS.UnityEngine.GameObject))
 	local go = ref:Instantiate()
+	UIRoot = go
 	go.name = "UI"
 	ref:Dispose()
-	Object.DontDestroyOnLoad(go)
+	CS.UnityEngine.Object.DontDestroyOnLoad(go)
 	local transform = go.transform
 	for i = 0, transform.childCount - 1 do
 		local childLayer = transform:GetChild(i)
@@ -31,15 +37,30 @@ function M.Init()
 			go = childLayer.gameObject,
 			name = name,
 			elements = {},
-			baseSortingOrder = canvas.sortingOrder,
+			currentOrder = canvas.sortingOrder,
 			canvas = canvas,
 			layerIndex = i
 		}
-		canvas.enabled = false
+		--canvas.enabled = false
 		local layerEnum = assert(UILayer.__CastFrom(name), name)
 		layers[layerEnum] = layer
 	end
+end
 
+function M.GetLayerRoot(name)
+	local layerEnum = assert(UILayer.__CastFrom(name), name)
+	return layers[layerEnum].transform
+end
+
+function M.GetLoadedUI(name)
+	local context = contexts[name]
+	if not context then
+		return
+	end
+	if context.status ~= "loaded" and context.status ~= "show" then
+		return
+	end
+	return context.view
 end
 
 function M.AfterSceneLoaded()
@@ -50,9 +71,12 @@ function M.SetUICamera(camera)
 	for _, layer in pairs(layers) do
 		layer.canvas.worldCamera = camera
 	end
+	local t = M.GetLayerRoot("Scene")
+	t.forward = -camera.transform.forward
 end
 
 local topFullScreenViewOrder = 0
+---@param element UI.UIViewContext
 function M._AddElement(element)
 	local order = element.view.Canvas.sortingOrder
 	local insertIndex
@@ -67,14 +91,36 @@ function M._AddElement(element)
 		table.insert(sortedElements, element)
 		insertIndex = #sortedElements
 	end
-	if element.configuration.FullScreen and order > topFullScreenViewOrder then
-		topFullScreenViewOrder = order
-		for i = 1, insertIndex - 1 do
-			local context = sortedElements[i]
-			context.view:SetVisible(false)
-			if context.bg then
-				context.bg:SetVisible(false)
+	if element.configuration.FullScreen then
+		if order > topFullScreenViewOrder then
+			topFullScreenViewOrder = order
+			for i = 1, insertIndex - 1 do
+				local context = sortedElements[i]
+				context.view:SetVisible(false)
+				if context.bg then
+					context.bg:SetVisible(false)
+				end
 			end
+		end
+	else
+		if element.configuration.CloseMethod == CloseOption.Outside then
+			assert(element.bg)
+			local EventBinding = SM.GetService(SM.SERVICE_TYPE.EVENT_BINDING)
+			local close; close = function()
+				EventBinding.RemoveEventListener("OnClick", element.bg.gameObject, close)
+				---@type CS.Extend.LuaBinding
+				local binding = element.view:GetComponent(typeof(CS.Extend.LuaBinding))
+				if binding then
+					local closeFunc = binding.LuaInstance.Close
+					if closeFunc then
+						closeFunc(binding.LuaInstance)
+						return
+					end
+				end
+
+				M.Hide(element)
+			end
+			EventBinding.AddEventListener("OnClick", element.bg.gameObject, close)
 		end
 	end
 end
@@ -88,7 +134,20 @@ function M._RemoveElement(element)
 			break
 		end
 	end
-	assert(index)
+	assert(index, element)
+	--[[ local showingCount = #sortedElements
+	if showingCount == 0 then
+		return
+	end
+	if showingCount == index - 1 then
+		local topElement = sortedElements[showingCount]
+		if not topElement.configuration.FullScreen then
+			if topElement.configuration.CloseMethod == CloseOption.AnyWhere then
+
+			end
+		end
+	end]]
+
 	if element.configuration.FullScreen and element.view.Canvas.sortingOrder >= topFullScreenViewOrder then
 		for i = index - 1, 1, -1 do
 			local context = sortedElements[i]
@@ -98,72 +157,49 @@ function M._RemoveElement(element)
 			end
 			if context.configuration.FullScreen then
 				topFullScreenViewOrder = context.view.Canvas.sortingOrder
-				break
+				return
 			end
 		end
+		topFullScreenViewOrder = 0
 	end
 end
 
-function M.Show(viewName, callback)
+function M.Load(viewName, callback)
+	if contexts[viewName] then
+		warn("ui view is exist : ", viewName)
+		return
+	end
+
 	local configuration = UIViewConfiguration:GetOne(viewName)
-	local context = { configuration = configuration }
-	local seq = sequence.new(function()
-		M._AddElement(context)
-	end, function(err)
-		error(err)
-		if context.transition then
-			AssetService.Recycle(context.transition)
-		end
-		if context.view then
-			AssetService.Recycle(context.view)
-		end
-		if context.bg then
-			AssetService.Recycle(context.bg)
-		end
-	end)
-	local behaviour = seq:build()
-	if configuration.Transition and configuration.Transition.GUIDValid then
-		behaviour:instantiate(configuration.Transition, layers[UILayer.MostTop].transform, function(go)
-			local view = go:GetComponent(UIViewBaseType)
-			view.Canvas.overrideSorting = true
-			view.Canvas.sortingOrder = layers[UILayer.MostTop].baseSortingOrder
-			context.transition = view
-			return view
-		end):view_show():wait_view_shown()
-	end
+	local context = UIViewContext.new(configuration)
+	context:Load(function(err, go)
+		callback(err, go)
+	end, layers)
+	contexts[viewName] = context
+	return context
+end
 
-	local layer = layers[configuration.AttachLayer]
-	table.insert(layer.elements, context)
-	behaviour:instantiate(configuration.UIView, layer.transform, function(go)
-		local view = go:GetComponent(UIViewBaseType)
-		view.Canvas.overrideSorting = true
-		view.Canvas.sortingOrder = layer.baseSortingOrder + #layer.elements * 2
-		context.view = view
-		callback(view)
-		return view
-	end):view_show(function()
-		if context.transition then
-			context.transition:Hide()
-			context.transition = nil
-		end
+function M.Show(viewName, callback)
+	local context;
+	context = M.Load(viewName, function(err, go)
+		context:Show()
+		callback(err, go)
 	end)
-
-	if not configuration.FullScreen then
-		if configuration.BackgroundFx and configuration.BackgroundFx.GUIDValid then
-			behaviour:instantiate(configuration.BackgroundFx, layer.transform, function(go)
-				local bg = go:GetComponent(UIViewBaseType)
-				bg.Canvas.overrideSorting = true
-				bg.Canvas.sortingOrder = context.view.Canvas.sortingOrder - 1
-				context.bg = bg
-				return bg
-			end)
-		end
-	end
-	behaviour:start()
 	return context
 end
 
 function M.Hide(context)
+	if type(context) == "string" then
+		local viewName = context
+		context = contexts[viewName]
+		if not context then
+			warn("View " .. viewName .. "not exist")
+			return
+		end
+		contexts[viewName] = nil
+	else
+		contexts[context.viewName] = nil
+	end
 	local layer = layers[context.configuration.AttachLayer]
 	for i, v in ipairs(layer.elements) do
 		if v == context then
@@ -172,10 +208,16 @@ function M.Hide(context)
 		end
 	end
 
-	context.view:Hide()
-	if context.bg then
-		context.bg:Hide()
-	end
+	context:Hide()
 	M._RemoveElement(context)
 end
+
+function M.clear()
+	for _, context in pairs(contexts) do
+		context:Destroy()
+	end
+	AssetService.Recycle(UIRoot)
+	contexts = nil
+end
+
 return M
---
+++ Lua/UI/UIViewContext.lua
@@ -0,0 +1,163 @@
+﻿---@class UI.UIViewContext
+---@field view CS.Extend.UI.UIViewBase
+---@field bg CS.Extend.UI.UIViewBase
+local M = class()
+local sequence = require("base.action.sequence")
+local UIViewBaseType = typeof(CS.Extend.UI.UIViewBase)
+---@type CS.Extend.Asset.AssetService
+local AssetService = CS.Extend.Asset.AssetService
+local UILayer = CS.Extend.UI.UILayer
+---@type CS.Extend.UI.CloseOption
+local CloseOption = CS.Extend.UI.CloseOption
+local SM = require("ServiceManager")
+
+---@param configuration CS.Extend.UI.UIViewConfiguration.Configuration
+function M:ctor(configuration)
+	self.configuration = configuration
+	self.viewName = configuration.Name
+	self.status = "none"
+end
+
+function M:Load(callback, layers)
+	self.status = "loading"
+	local seq = sequence.new(function()
+		self.status = "loaded"
+		callback(nil, self.view)
+	end, function(err)
+		error(err)
+		self:Destroy()
+		callback(err)
+	end)
+	local behaviour = seq:build()
+	if self.configuration.Transition and self.configuration.Transition.GUIDValid then
+		behaviour:instantiate_async(self.configuration.Transition, layers[UILayer.MostTop].transform, function(go)
+			local view = go:GetComponent(UIViewBaseType)
+			view.Canvas.overrideSorting = true
+			view.Canvas.sortingOrder = layers[UILayer.MostTop].currentOrder
+			self.transition = view
+			return view
+		end):view_show():wait_view_shown()
+	end
+
+	local layer = layers[self.configuration.AttachLayer]
+	local sortingOrder = layer.currentOrder + 2
+	layer.currentOrder = sortingOrder
+	if not self.configuration.FullScreen then
+		if self.configuration.BackgroundFx and self.configuration.BackgroundFx.GUIDValid then
+			behaviour:instantiate_async(self.configuration.BackgroundFx, layer.transform, function(go)
+				local bg = go:GetComponent(UIViewBaseType)
+				bg.Canvas.overrideSorting = true
+				bg.Canvas.sortingOrder = sortingOrder - 1
+				go:SetActive(false)
+				self.bg = bg
+				return bg
+			end)
+		end
+	end
+
+	table.insert(layer.elements, self)
+	behaviour:instantiate_async(self.configuration.UIView, layer.transform, function(go)
+		local view = go:GetComponent(UIViewBaseType)
+		view.Canvas.overrideSorting = true
+		view.Canvas.sortingOrder = sortingOrder
+		self.view = view
+		
+
+		if not self.configuration.FullScreen and self.configuration.CloseMethod == CloseOption.AnyWhere then
+			self.tapScreenHandle = CS.ScreenTouchUtil.RequestTapCallback(function()
+				self:Close()
+			end, 1, false)
+		end
+
+		if self.configuration.CloseMethod == CloseOption.Button then
+			local t = go.transform:Find(self.configuration.CloseButtonPath)
+			self.closeGO = t.gameObject
+			local EventBinding = SM.GetService(SM.SERVICE_TYPE.EVENT_BINDING)
+			EventBinding.AddEventListener("OnClick", self.closeGO, M.Close, self)
+		end
+		return view
+	end)
+	behaviour:start()
+end
+
+function M:Show()
+	assert(self.status == "loaded")
+	self.status = "show"
+	self.view:Show()
+	local uiService = SM.GetService(SM.SERVICE_TYPE.UI)
+	uiService._AddElement(self)
+
+	if self.transition then
+		self.transition:Hide(function()
+			AssetService.Recycle(self.transition)
+			self.configuration.Transition:Dispose()
+		end)
+	end
+	if self.bg then
+		self.bg:SetActive(true)
+		self.bg:Show()
+	end
+end
+
+function M:Hide()
+	assert(self.status == "show")
+	self.status = "hide"
+
+	local hiddenCallback; hiddenCallback = function()
+		self.view:Hidden("-", hiddenCallback)
+		self:Destroy()
+	end
+	self.view:Hidden("+", hiddenCallback)
+	if self.bg then
+		self.bg:Hide()
+	end
+	self.view:Hide()
+end
+
+function M:Close()
+	---@type CS.Extend.LuaBinding
+	local binding = self.view:GetComponent(typeof(CS.Extend.LuaBinding))
+	if binding then
+		local closeFunc = binding.LuaInstance.Close
+		if closeFunc then
+			closeFunc(binding.LuaInstance)
+			return
+		end
+	end
+	local uiService = SM.GetService(SM.SERVICE_TYPE.UI)
+	uiService.Hide(self)
+end
+
+function M:Destroy()
+	self.status = "destroyed"
+	if self.bg then
+		AssetService.Recycle(self.bg)
+		self.configuration.BackgroundFx:Dispose()
+	end
+
+	if self.view then
+		AssetService.Recycle(self.view)
+		self.configuration.UIView:Dispose()
+	end
+
+	if self.tapScreenHandle then
+		self.tapScreenHandle:Dispose()
+		self.tapScreenHandle = nil
+	end
+
+	if self.closeGO then
+		local EventBinding = SM.GetService(SM.SERVICE_TYPE.EVENT_BINDING)
+		EventBinding.RemoveEventListener("OnClick", self.closeGO, M.Close)
+		self.closeGO = nil
+	end
+end
+
+function M:SetVisible(visible)
+	self.view:SetVisible(visible)
+
+	if self.bg then
+		self.bg:SetVisible(visible)
+	end
+end
+
+return M
