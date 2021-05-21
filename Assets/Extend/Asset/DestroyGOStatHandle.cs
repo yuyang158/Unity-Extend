dfb7841119ac63e8e4820f43cf327d89da7c4022
yuyang158 <yuyang_158@hotmail.com>
2021-05-21 19:18:16 +0800

    hide in editor

--- Assets/Extend/Asset/DestroyGOStatHandle.cs
+++ Assets/Extend/Asset/DestroyGOStatHandle.cs
@@ -1,8 +1,14 @@
-﻿using Extend.Common;
+﻿using System;
+using Extend.Common;
 using UnityEngine;
 
 namespace Extend.Asset {
 	public class DestroyGOStatHandle : MonoBehaviour {
+#if UNITY_EDITOR
+		private void Awake() {
+			hideFlags = HideFlags.HideInInspector;
+		}
+#endif
 		private void OnDestroy() {
 			if( !CSharpServiceManager.Initialized )
 				return;
--- Assets/Extend/Asset/DirectDestroyGO.cs
+++ Assets/Extend/Asset/DirectDestroyGO.cs
@@ -1,5 +1,12 @@
-﻿namespace Extend.Asset {
+﻿using UnityEngine;
+
+namespace Extend.Asset {
 	public class DirectDestroyGO : AssetServiceManagedGO {
+#if UNITY_EDITOR
+		private void Awake() {
+			hideFlags = HideFlags.HideAndDontSave;
+		}
+#endif
 		internal override void Recycle() {
 			Destroy(gameObject);
 			base.Recycle();
