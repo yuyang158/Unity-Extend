1c20f1a1aa2b97e77221d8f17fdbf0986e67653b
yuyang <yuyang_158@hotmail.com>
2024-08-05 16:44:03 +0800

    Update make_ios_lua54.sh

--- xlua_build/make_ios_lua54.sh
+++ xlua_build/make_ios_lua54.sh
@@ -1,5 +1,5 @@
 mkdir -p build_ios_54 && cd build_ios_54
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.3 -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -DLUA_VERSION=5.4.3 -DLUA_USE_IOS -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
 cd ..
 cmake --build build_ios_54 --config Release
 mkdir -p plugin_lua54/Plugins/iOS/
